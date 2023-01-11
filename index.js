c246259e34d180e8f3ed61adf15945ff1dc79e33
Huy Tùng Nguyễn <tungnguyen151201@hotmail.com>
2023-01-12 00:30:31 +0700

    https

--- index.js
+++ index.js
@@ -7,40 +7,43 @@
 const app = require('./app');
 const port = 3001;
 const http = require('http');
+var https = require('https');
 
 const server = http.createServer(app);
+const httpsServer = https.createServer(app);
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
+server.listen(8080);
+httpsServer.listen(port);
+// server.on('error', onError);
+// server.on('listening', onListening);
 
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
+// function onError(error) {
+//   if (error.syscall !== 'listen') {
+//     throw error;
+//   }
 
-  const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+//   const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
+//   // handle specific listen errors with friendly messages
+//   switch (error.code) {
+//     case 'EACCES':
+//       console.error(bind + ' requires elevated privileges');
+//       process.exit(1);
+//       break;
+//     case 'EADDRINUSE':
+//       console.error(bind + ' is already in use');
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// }
 
-/**
- * Event listener for HTTP server "listening" event.
- */
+// /**
+//  * Event listener for HTTP server "listening" event.
+//  */
 
-function onListening() {
-  const addr = server.address();
-  console.log('Listening on localhost:' + addr.port);
-}
+// function onListening() {
+//   const addr = server.address();
+//   console.log('Listening on localhost:' + addr.port);
+// }
