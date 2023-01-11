4c540a416551c103bea96a1bfa07cd77c9bdea48
Huy Tùng Nguyễn <tungnguyen151201@hotmail.com>
2023-01-12 00:15:53 +0700

    index

--- server/index.js
+++ server/index.js
@@ -6,59 +6,12 @@
 
 const app = require('../app');
 const http = require('http');
-// const { Server } = require('socket.io');
-const chatServer = require('../routes/chat/createServer');
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || '3001');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
 
 const server = http.createServer(app);
-
-chatServer.attach(server, {
-  //<<==domain name for the nodejs server
-  cors: {
-    origin: 'http://localhost:3000',
-    methods: ['POST', 'GET'],
-  },
-});
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
 
 function onError(error) {
   if (error.syscall !== 'listen') {
